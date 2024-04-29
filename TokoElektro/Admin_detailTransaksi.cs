3003bf6c1c588ff3e7ce4cec0d6bd263c5d4f404
khilmeee <boskhilmiarked123@gmail.com>
2024-04-29 11:31:00 +0700

    menu

---
+++ TokoElektro/Admin_detailTransaksi.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TokoElektro
+{
+    public partial class Admin_detailTransaksi : UserControl
+    {
+        public Admin_detailTransaksi()
+        {
+            InitializeComponent();
+        }
+    }
+}
