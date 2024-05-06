571e9211419cd380eb20f19d09c06fcf06f45f1a
khilmeee <boskhilmiarked123@gmail.com>
2024-05-06 08:00:13 +0700

    Add Global Variable

---
+++ TokoElektro/GlobalVar.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TokoElektro
+{
+    internal class GlobalVar
+    {
+        public static string username { get; set; }
+        public static string name { get; set; }
+    }
+}
