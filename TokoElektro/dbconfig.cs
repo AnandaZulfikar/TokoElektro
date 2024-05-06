84ade50209ff305ee9bdc691e7a9775fa5c03dbb
khilmeee <boskhilmiarked123@gmail.com>
2024-05-06 07:59:54 +0700

    database configuration

---
+++ TokoElektro/dbconfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TokoElektro
+{
+    internal class dbconfig
+    {
+        static string dbname = "tokoelektro";
+        public static string conn = $"Data Source=MIWON\\SQLEXPRESS;Initial Catalog='{dbname}';Integrated Security=True;";
+    }
+}
