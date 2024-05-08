6e22e300362340ab0fbe0d800be8d03b1e79dfee
AnandaZulfikar <zulfikarstrike673@gmail.com>
2024-05-08 08:20:11 +0700

    - merubah tampilan karyawan barang

--- TokoElektro/Karyawan_Barang.Designer.cs
+++ TokoElektro/Karyawan_Barang.Designer.cs
@@ -55,94 +55,104 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(204, 106);
+            this.dataGridView1.Location = new System.Drawing.Point(273, 118);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(377, 315);
+            this.dataGridView1.Size = new System.Drawing.Size(294, 303);
             this.dataGridView1.TabIndex = 9;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
             // 
             // button_update
             // 
             this.button_update.BackColor = System.Drawing.Color.DeepSkyBlue;
             this.button_update.FlatAppearance.BorderSize = 0;
             this.button_update.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button_update.Location = new System.Drawing.Point(84, 426);
+            this.button_update.Location = new System.Drawing.Point(101, 398);
             this.button_update.Name = "button_update";
             this.button_update.Size = new System.Drawing.Size(75, 23);
             this.button_update.TabIndex = 14;
             this.button_update.Text = "Update";
             this.button_update.UseVisualStyleBackColor = false;
+            this.button_update.Click += new System.EventHandler(this.button_update_Click);
             // 
             // button_simpan
             // 
             this.button_simpan.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
             this.button_simpan.FlatAppearance.BorderSize = 0;
             this.button_simpan.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button_simpan.Location = new System.Drawing.Point(3, 426);
+            this.button_simpan.Location = new System.Drawing.Point(20, 398);
             this.button_simpan.Name = "button_simpan";
             this.button_simpan.Size = new System.Drawing.Size(75, 23);
             this.button_simpan.TabIndex = 13;
             this.button_simpan.Text = "Simpan";
             this.button_simpan.UseVisualStyleBackColor = false;
+            this.button_simpan.Click += new System.EventHandler(this.button_simpan_Click);
             // 
             // button_hapus
             // 
             this.button_hapus.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
             this.button_hapus.FlatAppearance.BorderSize = 0;
             this.button_hapus.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button_hapus.Location = new System.Drawing.Point(165, 426);
+            this.button_hapus.Location = new System.Drawing.Point(182, 398);
             this.button_hapus.Name = "button_hapus";
             this.button_hapus.Size = new System.Drawing.Size(75, 23);
             this.button_hapus.TabIndex = 12;
             this.button_hapus.Text = "Hapus";
             this.button_hapus.UseVisualStyleBackColor = false;
+            this.button_hapus.Click += new System.EventHandler(this.button_hapus_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(27, 111);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(27, 120);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(71, 13);
+            this.label2.Size = new System.Drawing.Size(89, 15);
             this.label2.TabIndex = 15;
-            this.label2.Text = "Nama barang";
+            this.label2.Text = "Nama barang :";
             // 
             // textBox_namaBarang
             // 
-            this.textBox_namaBarang.Location = new System.Drawing.Point(30, 133);
+            this.textBox_namaBarang.BackColor = System.Drawing.Color.LightGray;
+            this.textBox_namaBarang.Location = new System.Drawing.Point(30, 138);
             this.textBox_namaBarang.Name = "textBox_namaBarang";
-            this.textBox_namaBarang.Size = new System.Drawing.Size(100, 20);
+            this.textBox_namaBarang.Size = new System.Drawing.Size(212, 20);
             this.textBox_namaBarang.TabIndex = 16;
             // 
             // textBox_hargaBarang
             // 
-            this.textBox_hargaBarang.Location = new System.Drawing.Point(30, 197);
+            this.textBox_hargaBarang.BackColor = System.Drawing.Color.LightGray;
+            this.textBox_hargaBarang.Location = new System.Drawing.Point(30, 199);
             this.textBox_hargaBarang.Name = "textBox_hargaBarang";
-            this.textBox_hargaBarang.Size = new System.Drawing.Size(100, 20);
+            this.textBox_hargaBarang.Size = new System.Drawing.Size(212, 20);
             this.textBox_hargaBarang.TabIndex = 18;
             // 
             // label_hargaBarang
             // 
             this.label_hargaBarang.AutoSize = true;
-            this.label_hargaBarang.Location = new System.Drawing.Point(27, 175);
+            this.label_hargaBarang.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_hargaBarang.Location = new System.Drawing.Point(27, 181);
             this.label_hargaBarang.Name = "label_hargaBarang";
-            this.label_hargaBarang.Size = new System.Drawing.Size(72, 13);
+            this.label_hargaBarang.Size = new System.Drawing.Size(37, 15);
             this.label_hargaBarang.TabIndex = 17;
-            this.label_hargaBarang.Text = "Harga barang";
+            this.label_hargaBarang.Text = "Stok :";
             // 
             // textBox_stokBarang
             // 
-            this.textBox_stokBarang.Location = new System.Drawing.Point(30, 260);
+            this.textBox_stokBarang.BackColor = System.Drawing.Color.LightGray;
+            this.textBox_stokBarang.Location = new System.Drawing.Point(30, 265);
             this.textBox_stokBarang.Name = "textBox_stokBarang";
-            this.textBox_stokBarang.Size = new System.Drawing.Size(100, 20);
+            this.textBox_stokBarang.Size = new System.Drawing.Size(212, 20);
             this.textBox_stokBarang.TabIndex = 20;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(27, 238);
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(27, 247);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(65, 13);
+            this.label4.Size = new System.Drawing.Size(47, 15);
             this.label4.TabIndex = 19;
-            this.label4.Text = "Stok barang";
+            this.label4.Text = "Harga :";
             // 
             // Karyawan_Barang
             // 
