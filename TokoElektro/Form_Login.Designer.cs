afc4610a884f77f3b8c936494c43951d24c40e52
AnandaZulfikar <zulfikarstrike673@gmail.com>
2024-05-08 08:19:25 +0700

    - menambah password char form login

--- TokoElektro/Form_Login.Designer.cs
+++ TokoElektro/Form_Login.Designer.cs
@@ -90,6 +90,7 @@
             // 
             this.textBox_password.Location = new System.Drawing.Point(237, 141);
             this.textBox_password.Name = "textBox_password";
+            this.textBox_password.PasswordChar = '*';
             this.textBox_password.Size = new System.Drawing.Size(173, 20);
             this.textBox_password.TabIndex = 3;
             // 
