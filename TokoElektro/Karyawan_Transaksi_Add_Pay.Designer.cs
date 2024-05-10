20f800033e7065f8a5b2517a12083592ac6669af
khilmeee <boskhilmiarked123@gmail.com>
2024-05-10 18:29:09 +0700

    pembayaran proses

---
+++ TokoElektro/Karyawan_Transaksi_Add_Pay.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace TokoElektro
+{
+    partial class Karyawan_Transaksi_Add_Pay
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_nominal_bayar = new System.Windows.Forms.TextBox();
+            this.label_total = new System.Windows.Forms.Label();
+            this.button_uang_pas = new System.Windows.Forms.Button();
+            this.button_uang_50k = new System.Windows.Forms.Button();
+            this.button_uang_200k = new System.Windows.Forms.Button();
+            this.button_uang_100k = new System.Windows.Forms.Button();
+            this.button_proses = new System.Windows.Forms.Button();
+            this.button_kembali = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(24, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(171, 16);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "WELCOME KARYAWAN";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(183, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(206, 25);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Pembayaran - Tunai";
+            // 
+            // textBox_nominal_bayar
+            // 
+            this.textBox_nominal_bayar.Location = new System.Drawing.Point(213, 150);
+            this.textBox_nominal_bayar.Name = "textBox_nominal_bayar";
+            this.textBox_nominal_bayar.Size = new System.Drawing.Size(137, 20);
+            this.textBox_nominal_bayar.TabIndex = 18;
+            // 
+            // label_total
+            // 
+            this.label_total.AutoSize = true;
+            this.label_total.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_total.Location = new System.Drawing.Point(210, 131);
+            this.label_total.Name = "label_total";
+            this.label_total.Size = new System.Drawing.Size(60, 16);
+            this.label_total.TabIndex = 19;
+            this.label_total.Text = "Total : -";
+            // 
+            // button_uang_pas
+            // 
+            this.button_uang_pas.Location = new System.Drawing.Point(198, 192);
+            this.button_uang_pas.Name = "button_uang_pas";
+            this.button_uang_pas.Size = new System.Drawing.Size(75, 23);
+            this.button_uang_pas.TabIndex = 20;
+            this.button_uang_pas.Text = "Uang Pas";
+            this.button_uang_pas.UseVisualStyleBackColor = true;
+            this.button_uang_pas.Click += new System.EventHandler(this.button_uang_pas_Click);
+            // 
+            // button_uang_50k
+            // 
+            this.button_uang_50k.Location = new System.Drawing.Point(293, 192);
+            this.button_uang_50k.Name = "button_uang_50k";
+            this.button_uang_50k.Size = new System.Drawing.Size(75, 23);
+            this.button_uang_50k.TabIndex = 21;
+            this.button_uang_50k.Text = "Rp. 50.000";
+            this.button_uang_50k.UseVisualStyleBackColor = true;
+            this.button_uang_50k.Click += new System.EventHandler(this.button_uang_50k_Click);
+            // 
+            // button_uang_200k
+            // 
+            this.button_uang_200k.Location = new System.Drawing.Point(293, 232);
+            this.button_uang_200k.Name = "button_uang_200k";
+            this.button_uang_200k.Size = new System.Drawing.Size(75, 23);
+            this.button_uang_200k.TabIndex = 23;
+            this.button_uang_200k.Text = "Rp. 200.000";
+            this.button_uang_200k.UseVisualStyleBackColor = true;
+            this.button_uang_200k.Click += new System.EventHandler(this.button_uang_200k_Click);
+            // 
+            // button_uang_100k
+            // 
+            this.button_uang_100k.Location = new System.Drawing.Point(198, 232);
+            this.button_uang_100k.Name = "button_uang_100k";
+            this.button_uang_100k.Size = new System.Drawing.Size(75, 23);
+            this.button_uang_100k.TabIndex = 22;
+            this.button_uang_100k.Text = "Rp. 100.000";
+            this.button_uang_100k.UseVisualStyleBackColor = true;
+            this.button_uang_100k.Click += new System.EventHandler(this.button_uang_100k_Click);
+            // 
+            // button_proses
+            // 
+            this.button_proses.Location = new System.Drawing.Point(198, 273);
+            this.button_proses.Name = "button_proses";
+            this.button_proses.Size = new System.Drawing.Size(170, 37);
+            this.button_proses.TabIndex = 24;
+            this.button_proses.Text = "Proses";
+            this.button_proses.UseVisualStyleBackColor = true;
+            this.button_proses.Click += new System.EventHandler(this.button_proses_Click);
+            // 
+            // button_kembali
+            // 
+            this.button_kembali.Location = new System.Drawing.Point(27, 412);
+            this.button_kembali.Name = "button_kembali";
+            this.button_kembali.Size = new System.Drawing.Size(75, 23);
+            this.button_kembali.TabIndex = 25;
+            this.button_kembali.Text = "Kembali";
+            this.button_kembali.UseVisualStyleBackColor = true;
+            this.button_kembali.Click += new System.EventHandler(this.button_kembali_Click);
+            // 
+            // Karyawan_Transaksi_Add_Pay
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.button_kembali);
+            this.Controls.Add(this.button_proses);
+            this.Controls.Add(this.button_uang_200k);
+            this.Controls.Add(this.button_uang_100k);
+            this.Controls.Add(this.button_uang_50k);
+            this.Controls.Add(this.button_uang_pas);
+            this.Controls.Add(this.label_total);
+            this.Controls.Add(this.textBox_nominal_bayar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Karyawan_Transaksi_Add_Pay";
+            this.Size = new System.Drawing.Size(595, 453);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_nominal_bayar;
+        private System.Windows.Forms.Label label_total;
+        private System.Windows.Forms.Button button_uang_pas;
+        private System.Windows.Forms.Button button_uang_50k;
+        private System.Windows.Forms.Button button_uang_200k;
+        private System.Windows.Forms.Button button_uang_100k;
+        private System.Windows.Forms.Button button_proses;
+        private System.Windows.Forms.Button button_kembali;
+    }
+}
---
+++ TokoElektro/Karyawan_Transaksi_Add_Pay.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TokoElektro
+{
+    public partial class Karyawan_Transaksi_Add_Pay : UserControl, IAcceptParameters
+    {
+        public SqlConnection connection = new SqlConnection(dbconfig.conn);
+        public SqlCommand command;
+        private int idstruk,total;
+        public Karyawan_Transaksi_Add_Pay()
+        {
+            InitializeComponent();
+        }
+
+        private void button_kembali_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void button_proses_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBox_nominal_bayar.Text))
+            {
+                int bayar = Convert.ToInt32(textBox_nominal_bayar.Text);
+
+                if (bayar < total)
+                {
+                    MessageBox.Show("Uangnya kurang");
+                    return;
+                }
+                else
+                {
+                    try
+                    {
+                        // Perbarui data struk dengan nilai bayar yang baru
+                        connection.Open();
+                        string queryUpdateStruk = $"UPDATE struk SET bayar = {bayar}, kembali = {bayar - total} WHERE id = {idstruk}";
+                        SqlCommand commandUpdateStruk = new SqlCommand(queryUpdateStruk, connection);
+                        commandUpdateStruk.ExecuteNonQuery();
+
+                        MessageBox.Show("Transaksi berhasil diproses");
+                        this.Hide();
+                        connection.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error: " + ex.Message);
+                        connection.Close();
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Silakan masukkan nominal pembayaran");
+            }
+        }
+
+        private void button_uang_pas_Click(object sender, EventArgs e)
+        {
+            textBox_nominal_bayar.Text = total.ToString();
+        }
+
+        private void button_uang_50k_Click(object sender, EventArgs e)
+        {
+            textBox_nominal_bayar.Text = "50000";
+        }
+
+        private void button_uang_100k_Click(object sender, EventArgs e)
+        {
+            textBox_nominal_bayar.Text = "100000";
+        }
+
+        private void button_uang_200k_Click(object sender, EventArgs e)
+        {
+            textBox_nominal_bayar.Text = "200000";
+        }
+
+        public void SetParameters(params object[] parameters)
+        {
+            idstruk = Convert.ToInt32(parameters[0].ToString());
+            total = Convert.ToInt32(parameters[1].ToString());
+            label_total.Text = $"Total : {total.ToString("C", new CultureInfo("id-ID"))}";
+        }
+    }
+}
