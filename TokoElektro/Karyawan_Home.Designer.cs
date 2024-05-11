f1d53bffed911c2418be8312033086eb5cbbfddb
khilmeee <boskhilmiarked123@gmail.com>
2024-05-11 13:35:36 +0700

    nampilin jumlah barang dan struk di home

--- TokoElektro/Karyawan_Home.Designer.cs
+++ TokoElektro/Karyawan_Home.Designer.cs
@@ -30,30 +30,24 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Karyawan_Home));
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label_total = new System.Windows.Forms.Label();
+            this.label_total_barang = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.panel4 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.panel3 = new System.Windows.Forms.Panel();
             this.panel5 = new System.Windows.Forms.Panel();
-            this.label7 = new System.Windows.Forms.Label();
+            this.label_total_struk = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
             this.panel6 = new System.Windows.Forms.Panel();
             this.panel1.SuspendLayout();
-            this.panel2.SuspendLayout();
             this.panel5.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.panel1.Controls.Add(this.label_total);
+            this.panel1.Controls.Add(this.label_total_barang);
             this.panel1.Controls.Add(this.label3);
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.panel4);
@@ -62,16 +56,16 @@
             this.panel1.Size = new System.Drawing.Size(241, 137);
             this.panel1.TabIndex = 7;
             // 
-            // label_total
+            // label_total_barang
             // 
-            this.label_total.AutoSize = true;
-            this.label_total.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label_total.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label_total.Location = new System.Drawing.Point(128, 77);
-            this.label_total.Name = "label_total";
-            this.label_total.Size = new System.Drawing.Size(55, 29);
-            this.label_total.TabIndex = 3;
-            this.label_total.Text = "150";
+            this.label_total_barang.AutoSize = true;
+            this.label_total_barang.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_total_barang.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label_total_barang.Location = new System.Drawing.Point(128, 77);
+            this.label_total_barang.Name = "label_total_barang";
+            this.label_total_barang.Size = new System.Drawing.Size(55, 29);
+            this.label_total_barang.TabIndex = 3;
+            this.label_total_barang.Text = "150";
             // 
             // label3
             // 
@@ -114,82 +108,28 @@
             this.label1.TabIndex = 6;
             this.label1.Text = "WELCOME KARYAWAN";
             // 
-            // panel2
-            // 
-            this.panel2.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.panel2.Controls.Add(this.label4);
-            this.panel2.Controls.Add(this.label5);
-            this.panel2.Controls.Add(this.label6);
-            this.panel2.Controls.Add(this.panel3);
-            this.panel2.Location = new System.Drawing.Point(321, 82);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(241, 137);
-            this.panel2.TabIndex = 8;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label4.Location = new System.Drawing.Point(128, 77);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(55, 29);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "150";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label5.Location = new System.Drawing.Point(103, 49);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(56, 18);
-            this.label5.TabIndex = 2;
-            this.label5.Text = "Total :";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label6.Location = new System.Drawing.Point(6, 8);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(127, 29);
-            this.label6.TabIndex = 1;
-            this.label6.Text = "Transaksi";
-            // 
-            // panel3
-            // 
-            this.panel3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel3.BackgroundImage")));
-            this.panel3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.panel3.Location = new System.Drawing.Point(3, 49);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(94, 85);
-            this.panel3.TabIndex = 0;
-            // 
             // panel5
             // 
             this.panel5.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.panel5.Controls.Add(this.label7);
+            this.panel5.Controls.Add(this.label_total_struk);
             this.panel5.Controls.Add(this.label8);
             this.panel5.Controls.Add(this.label9);
             this.panel5.Controls.Add(this.panel6);
-            this.panel5.Location = new System.Drawing.Point(30, 265);
+            this.panel5.Location = new System.Drawing.Point(327, 82);
             this.panel5.Name = "panel5";
             this.panel5.Size = new System.Drawing.Size(241, 137);
             this.panel5.TabIndex = 9;
             // 
-            // label7
+            // label_total_struk
             // 
-            this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label7.Location = new System.Drawing.Point(128, 77);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(55, 29);
-            this.label7.TabIndex = 3;
-            this.label7.Text = "150";
+            this.label_total_struk.AutoSize = true;
+            this.label_total_struk.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_total_struk.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label_total_struk.Location = new System.Drawing.Point(128, 77);
+            this.label_total_struk.Name = "label_total_struk";
+            this.label_total_struk.Size = new System.Drawing.Size(55, 29);
+            this.label_total_struk.TabIndex = 3;
+            this.label_total_struk.Text = "150";
             // 
             // label8
             // 
@@ -228,14 +168,11 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel5);
             this.Name = "Karyawan_Home";
             this.Size = new System.Drawing.Size(595, 453);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
-            this.panel2.ResumeLayout(false);
-            this.panel2.PerformLayout();
             this.panel5.ResumeLayout(false);
             this.panel5.PerformLayout();
             this.ResumeLayout(false);
@@ -246,18 +183,13 @@
         #endregion
 
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Label label_total;
+        private System.Windows.Forms.Label label_total_barang;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Panel panel4;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.Panel panel5;
-        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label_total_struk;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.Panel panel6;
