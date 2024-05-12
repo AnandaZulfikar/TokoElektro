cf24b676eb7eaec9b8c5b38471e166ee6b1e35b2
AnandaZulfikar <zulfikarstrike673@gmail.com>
2024-05-12 22:58:13 +0700

    func jumlah barang, struk, karyawan

--- TokoElektro/Admin_Home.cs
+++ TokoElektro/Admin_Home.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -18,13 +19,57 @@ namespace TokoElektro
             panel1.Region = System.Drawing.Region.FromHrgn(Rounded.CreateRoundRectRgn(0, 0, panel1.Width, panel1.Height, 20, 20));
             panel2.Region = System.Drawing.Region.FromHrgn(Rounded.CreateRoundRectRgn(0, 0, panel2.Width, panel2.Height, 20, 20));
             panel5.Region = System.Drawing.Region.FromHrgn(Rounded.CreateRoundRectRgn(0, 0, panel5.Width, panel5.Height, 20,20));
-
+            showTotal();
         }
 
-
-        private void label2_Click(object sender, EventArgs e)
+        public void showTotal()
         {
+            try
+            {
+                string queryTotalBarang = "SELECT COUNT(*) FROM barang";
+                int totalBarang = 0;
 
+                using (SqlConnection connection = new SqlConnection(dbconfig.conn))
+                {
+                    using (SqlCommand command = new SqlCommand(queryTotalBarang, connection))
+                    {
+                        connection.Open();
+                        totalBarang = (int)command.ExecuteScalar();
+                    }
+                }
+
+                string queryTotalTransaksi = "SELECT COUNT(*) FROM struk WHERE bayar > 0";
+                int totalTransaksi = 0;
+
+                using (SqlConnection connection = new SqlConnection(dbconfig.conn))
+                {
+                    using (SqlCommand command = new SqlCommand(queryTotalTransaksi, connection))
+                    {
+                        connection.Open();
+                        totalTransaksi = (int)command.ExecuteScalar();
+                    }
+                }
+
+                string queryTotalKaryawan = "SELECT COUNT(*) FROM karyawan";
+                int totalKaryawan = 0;
+
+                using (SqlConnection connection = new SqlConnection(dbconfig.conn))
+                {
+                    using (SqlCommand command = new SqlCommand(queryTotalKaryawan, connection))
+                    {
+                        connection.Open();
+                        totalKaryawan = (int)command.ExecuteScalar();
+                    }
+                }
+
+                label_total_barang.Text = totalBarang.ToString();
+                label_total_struk.Text = totalTransaksi.ToString();
+                label_total_karyawan.Text = totalKaryawan.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
         }
     }
 }
